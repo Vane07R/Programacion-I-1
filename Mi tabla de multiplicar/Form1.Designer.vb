e691d820e1c653b8fde62db311b6528672510708
USIS009220 <68669293+USIS009220@users.noreply.github.com>
2020-08-12 12:04:31 -0600

    Le he agragado lo que faltaba en este trabajo

--- Mi tabla de multiplicar/Form1.Designer.vb
+++ Mi tabla de multiplicar/Form1.Designer.vb
@@ -22,10 +22,63 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblntabla = New System.Windows.Forms.Label()
+        Me.txtntabla = New System.Windows.Forms.TextBox()
+        Me.btnProcesar = New System.Windows.Forms.Button()
+        Me.lstTabla = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'lblntabla
+        '
+        Me.lblntabla.AutoSize = True
+        Me.lblntabla.Location = New System.Drawing.Point(183, 73)
+        Me.lblntabla.Name = "lblntabla"
+        Me.lblntabla.Size = New System.Drawing.Size(37, 13)
+        Me.lblntabla.TabIndex = 0
+        Me.lblntabla.Text = "Tabla:"
+        '
+        'txtntabla
+        '
+        Me.txtntabla.Location = New System.Drawing.Point(226, 70)
+        Me.txtntabla.Name = "txtntabla"
+        Me.txtntabla.Size = New System.Drawing.Size(136, 20)
+        Me.txtntabla.TabIndex = 1
+        '
+        'btnProcesar
+        '
+        Me.btnProcesar.Location = New System.Drawing.Point(177, 133)
+        Me.btnProcesar.Name = "btnProcesar"
+        Me.btnProcesar.Size = New System.Drawing.Size(209, 87)
+        Me.btnProcesar.TabIndex = 2
+        Me.btnProcesar.Text = "Generar Tabla"
+        Me.btnProcesar.UseVisualStyleBackColor = True
+        '
+        'lstTabla
+        '
+        Me.lstTabla.FormattingEnabled = True
+        Me.lstTabla.Location = New System.Drawing.Point(407, 73)
+        Me.lstTabla.Name = "lstTabla"
+        Me.lstTabla.Size = New System.Drawing.Size(234, 147)
+        Me.lstTabla.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.lstTabla)
+        Me.Controls.Add(Me.btnProcesar)
+        Me.Controls.Add(Me.txtntabla)
+        Me.Controls.Add(Me.lblntabla)
+        Me.Name = "Form1"
+        Me.Text = "Tabla de Multiplicar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents lblntabla As Label
+    Friend WithEvents txtntabla As TextBox
+    Friend WithEvents btnProcesar As Button
+    Friend WithEvents lstTabla As ListBox
 End Class
--- Mi tabla de multiplicar/Form1.vb
+++ Mi tabla de multiplicar/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        Dim ntabla As SByte
+        ntabla = txtntabla.Text
 
+        lstTabla.Items.Clear()
+        For i = 1 To 9
+            lstTabla.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
+        Next
+    End Sub
 End Class
