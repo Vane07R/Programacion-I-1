f5baefe3e13cea5d3dec1572d455fa25a0cdf1e4
USIS009220 <68669293+USIS009220@users.noreply.github.com>
2020-08-14 14:22:20 -0600

    He agregado las estructuras de control

--- Mi tabla de multiplicar/Form1.Designer.vb
+++ Mi tabla de multiplicar/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,65 +20,104 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.lblntabla = New System.Windows.Forms.Label()
-        Me.txtntabla = New System.Windows.Forms.TextBox()
         Me.btnProcesar = New System.Windows.Forms.Button()
-        Me.lstTabla = New System.Windows.Forms.ListBox()
+        Me.lstejercicios = New System.Windows.Forms.ListBox()
+        Me.btnparimpar = New System.Windows.Forms.Button()
+        Me.txtnum = New System.Windows.Forms.TextBox()
+        Me.lblnum = New System.Windows.Forms.Label()
+        Me.btnPrimo = New System.Windows.Forms.Button()
+        Me.btnCajero = New System.Windows.Forms.Button()
         Me.SuspendLayout()
-        '
-        'lblntabla
-        '
-        Me.lblntabla.AutoSize = True
-        Me.lblntabla.Location = New System.Drawing.Point(183, 73)
-        Me.lblntabla.Name = "lblntabla"
-        Me.lblntabla.Size = New System.Drawing.Size(37, 13)
-        Me.lblntabla.TabIndex = 0
-        Me.lblntabla.Text = "Tabla:"
-        '
-        'txtntabla
-        '
-        Me.txtntabla.Location = New System.Drawing.Point(226, 70)
-        Me.txtntabla.Name = "txtntabla"
-        Me.txtntabla.Size = New System.Drawing.Size(136, 20)
-        Me.txtntabla.TabIndex = 1
         '
         'btnProcesar
         '
-        Me.btnProcesar.Location = New System.Drawing.Point(177, 133)
+        Me.btnProcesar.Location = New System.Drawing.Point(448, 265)
         Me.btnProcesar.Name = "btnProcesar"
-        Me.btnProcesar.Size = New System.Drawing.Size(209, 87)
-        Me.btnProcesar.TabIndex = 2
-        Me.btnProcesar.Text = "Generar Tabla"
+        Me.btnProcesar.Size = New System.Drawing.Size(162, 35)
+        Me.btnProcesar.TabIndex = 0
+        Me.btnProcesar.Text = "Numeros pares"
         Me.btnProcesar.UseVisualStyleBackColor = True
         '
-        'lstTabla
+        'lstejercicios
         '
-        Me.lstTabla.FormattingEnabled = True
-        Me.lstTabla.Location = New System.Drawing.Point(407, 73)
-        Me.lstTabla.Name = "lstTabla"
-        Me.lstTabla.Size = New System.Drawing.Size(234, 147)
-        Me.lstTabla.TabIndex = 3
+        Me.lstejercicios.FormattingEnabled = True
+        Me.lstejercicios.Location = New System.Drawing.Point(448, 60)
+        Me.lstejercicios.Name = "lstejercicios"
+        Me.lstejercicios.Size = New System.Drawing.Size(162, 199)
+        Me.lstejercicios.TabIndex = 1
+        '
+        'btnparimpar
+        '
+        Me.btnparimpar.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnparimpar.Location = New System.Drawing.Point(12, 223)
+        Me.btnparimpar.Name = "btnparimpar"
+        Me.btnparimpar.Size = New System.Drawing.Size(127, 51)
+        Me.btnparimpar.TabIndex = 2
+        Me.btnparimpar.Text = "Par o Impar?"
+        Me.btnparimpar.UseVisualStyleBackColor = True
+        '
+        'txtnum
+        '
+        Me.txtnum.Location = New System.Drawing.Point(157, 148)
+        Me.txtnum.Name = "txtnum"
+        Me.txtnum.Size = New System.Drawing.Size(115, 20)
+        Me.txtnum.TabIndex = 3
+        '
+        'lblnum
+        '
+        Me.lblnum.AutoSize = True
+        Me.lblnum.Location = New System.Drawing.Point(101, 148)
+        Me.lblnum.Name = "lblnum"
+        Me.lblnum.Size = New System.Drawing.Size(50, 13)
+        Me.lblnum.TabIndex = 4
+        Me.lblnum.Text = "Numero: "
+        '
+        'btnPrimo
+        '
+        Me.btnPrimo.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrimo.Location = New System.Drawing.Point(145, 223)
+        Me.btnPrimo.Name = "btnPrimo"
+        Me.btnPrimo.Size = New System.Drawing.Size(127, 51)
+        Me.btnPrimo.TabIndex = 5
+        Me.btnPrimo.Text = "Primo?"
+        Me.btnPrimo.UseVisualStyleBackColor = True
+        '
+        'btnCajero
+        '
+        Me.btnCajero.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCajero.Location = New System.Drawing.Point(278, 223)
+        Me.btnCajero.Name = "btnCajero"
+        Me.btnCajero.Size = New System.Drawing.Size(127, 51)
+        Me.btnCajero.TabIndex = 6
+        Me.btnCajero.Text = "Cajero"
+        Me.btnCajero.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.lstTabla)
+        Me.Controls.Add(Me.btnCajero)
+        Me.Controls.Add(Me.btnPrimo)
+        Me.Controls.Add(Me.lblnum)
+        Me.Controls.Add(Me.txtnum)
+        Me.Controls.Add(Me.btnparimpar)
+        Me.Controls.Add(Me.lstejercicios)
         Me.Controls.Add(Me.btnProcesar)
-        Me.Controls.Add(Me.txtntabla)
-        Me.Controls.Add(Me.lblntabla)
         Me.Name = "Form1"
-        Me.Text = "Tabla de Multiplicar"
+        Me.Text = "Estructuras de control"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents lblntabla As Label
-    Friend WithEvents txtntabla As TextBox
     Friend WithEvents btnProcesar As Button
-    Friend WithEvents lstTabla As ListBox
+    Friend WithEvents lstejercicios As ListBox
+    Friend WithEvents btnparimpar As Button
+    Friend WithEvents txtnum As TextBox
+    Friend WithEvents lblnum As Label
+    Friend WithEvents btnPrimo As Button
+    Friend WithEvents btnCajero As Button
 End Class
--- Mi tabla de multiplicar/Form1.vb
+++ Mi tabla de multiplicar/Form1.vb
@@ -1,11 +1,62 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
-        Dim ntabla As SByte
-        ntabla = txtntabla.Text
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        lstejercicios.Items.Clear()
 
-        lstTabla.Items.Clear()
-        For i = 1 To 9
-            lstTabla.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
+        lstejercicios.Items.Add("Numero pares del 1 al 20")
+        For i = 2 To 20 Step 2
+            lstejercicios.Items.Add(i)
+        Next
+
+        lstejercicios.Items.Add("Numero impares del 1 al 20")
+        For i = 1 To 20 Step 2
+            lstejercicios.Items.Add(i)
+        Next
+    End Sub
+
+    Private Sub btnparimpar_Click(sender As Object, e As EventArgs) Handles btnparimpar.Click
+        Dim num = txtnum.Text
+        If num Mod 2 = 0 Then
+            MessageBox.Show("El numero: " + num + ", es par")
+        Else
+            MessageBox.Show("El numero: " + num + ", es impar")
+        End If
+    End Sub
+
+    Private Sub btnPrimo_Click(sender As Object, e As EventArgs) Handles btnPrimo.Click
+        Dim num = txtnum.Text, i = 0, acu = 1
+        While i < 3 And acu <= num
+            If num Mod acu = 0 Then
+                i += 1 ' i=i+1
+            End If
+            acu += 1
+        End While
+        If i <= 2 Then
+            MessageBox.Show("Es primo")
+        Else
+            MessageBox.Show("NO es primo")
+        End If
+    End Sub
+
+    Private Sub btnCajero_Click(sender As Object, e As EventArgs) Handles btnCajero.Click
+        Dim denominaciones() = {100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.1, 0.05, 0.01}, valor = txtnum.Text, cantidad = 0
+        Dim denomina As String
+
+        lstejercicios.Items.Clear()
+
+        For Each denominacion In denominaciones
+            While valor > 0 And denominacion <= valor
+                valor = Math.Round(valor - denominacion, 2)
+                cantidad += 1
+            End While
+            If cantidad > 0 Then
+                If denominacion <= 1 Then
+                    denomina = " moneda de "
+                Else
+                    denomina = " billete de "
+                End If
+                lstejercicios.Items.Add(cantidad.ToString() + denomina + denominacion.ToString())
+                cantidad = 0
+            End If
         Next
     End Sub
 End Class
