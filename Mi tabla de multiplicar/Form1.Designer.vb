1462deaa281e0f0a95159770b043c15c9ca22ca9
USIS009220 <68669293+USIS009220@users.noreply.github.com>
2020-08-28 20:59:13 -0600

    He cambiado el tamaño

--- Mi tabla de multiplicar/Form1.Designer.vb
+++ Mi tabla de multiplicar/Form1.Designer.vb
@@ -31,7 +31,7 @@ Partial Class Form1
         'lblntabla
         '
         Me.lblntabla.AutoSize = True
-        Me.lblntabla.Location = New System.Drawing.Point(183, 73)
+        Me.lblntabla.Location = New System.Drawing.Point(52, 61)
         Me.lblntabla.Name = "lblntabla"
         Me.lblntabla.Size = New System.Drawing.Size(37, 13)
         Me.lblntabla.TabIndex = 0
@@ -39,14 +39,14 @@ Partial Class Form1
         '
         'txtntabla
         '
-        Me.txtntabla.Location = New System.Drawing.Point(226, 70)
+        Me.txtntabla.Location = New System.Drawing.Point(95, 58)
         Me.txtntabla.Name = "txtntabla"
         Me.txtntabla.Size = New System.Drawing.Size(136, 20)
         Me.txtntabla.TabIndex = 1
         '
         'btnProcesar
         '
-        Me.btnProcesar.Location = New System.Drawing.Point(177, 133)
+        Me.btnProcesar.Location = New System.Drawing.Point(46, 121)
         Me.btnProcesar.Name = "btnProcesar"
         Me.btnProcesar.Size = New System.Drawing.Size(209, 87)
         Me.btnProcesar.TabIndex = 2
@@ -56,7 +56,7 @@ Partial Class Form1
         'lstTabla
         '
         Me.lstTabla.FormattingEnabled = True
-        Me.lstTabla.Location = New System.Drawing.Point(407, 73)
+        Me.lstTabla.Location = New System.Drawing.Point(276, 61)
         Me.lstTabla.Name = "lstTabla"
         Me.lstTabla.Size = New System.Drawing.Size(234, 147)
         Me.lstTabla.TabIndex = 3
@@ -65,7 +65,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(563, 307)
         Me.Controls.Add(Me.lstTabla)
         Me.Controls.Add(Me.btnProcesar)
         Me.Controls.Add(Me.txtntabla)
